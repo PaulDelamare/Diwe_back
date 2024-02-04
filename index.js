9f7f90c3fd5f3816f1ef0c996e7d4c525ae08750
Paul_Delamare <paulodela2006@gmail.com>
2024-02-04 16:16:54 +0100

    Create auth route

--- index.js
+++ index.js
@@ -5,28 +5,34 @@ const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
 
+//Require route
+const AuthRoute = require('./routes/auth.routes');
+
 //
 //////////
 
 //////////
 //VARIABLE
 
-const port = process.env.JWT_SECRET
+const port = process.env.PORT
 const app = express();
 const mongoose = require('mongoose');
 
 const passport = require('passport');
-// require('./config/passport-config')(passport);
+require('./config/passport-config')(passport);
 
 //
 //////////
-
-
 
 //////////
 //CONFIG API
 
 //Removes cors problems
+// const corsOptions = {
+//     origin: 'web site link',
+//     optionsSuccessStatus: 200,
+// };
+
 app.use(cors());
 
 //Specify that the api will be in json
@@ -46,6 +52,7 @@ app.use(passport.initialize());
 //////////
 //API ROUTES
 
+app.use('/api', AuthRoute);
 
 //////////
 //////////
@@ -53,7 +60,7 @@ app.use(passport.initialize());
 //////////
 //DATABASE CONNECTION
 mongoose.connect(process.env.MONGO_URI)
-.then((result)=> app.listen(process.env.PORT,()=>{
+.then((result)=> app.listen(port,()=>{
     console.log('Server running');
 }))
 .catch((err)=> console.log(err));
