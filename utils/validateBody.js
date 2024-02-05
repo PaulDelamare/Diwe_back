1b3f41e1517bcb122004dbb3c66f459d06124202
Paul_Delamare <paulodela2006@gmail.com>
2024-02-05 23:28:52 +0100

    Add methods description and checkPassword

--- utils/validateBody.js
+++ utils/validateBody.js
@@ -2,35 +2,37 @@
 //REQUIRE
 const { validationResult, check } = require('express-validator');
 const User = require('../models/User');
-
+const bcrypt = require('bcryptjs');
 //////////
 //////////
-
-
-// exports.validateBody = async (validationRules, req, res, notFoundMessage = "") => {
-
-//     // Asynchronously apply all validation rules to the query fields
-//     await Promise.all(validationRules.map(validationRule => validationRule.run(req)));
-
-//     // Get the validation results for the query
-//     return validationResult(req);
-// };
 
 //////////
 //CLASS
 class ValidateBody{
+    
     // Validation rules array
-    validationRules = [];
+    _validationRules = [];
 
     //////////
     //VALIDATION METHODS
 
     // Text validator
+    
+/**
++     * Validates the input text based on the provided rules.
++     *
++     * @param {string} text - the key in body
++     * @param {boolean} require - required in body
++     * @param {number} min - the minimum length of the text
++     * @param {number} max - the maximum length of the text
++     * @param {string} traduction - the translation of the text for error messages
++     * @return {void} 
++     */
     textValidator(text, require, min, max, traduction){
         //Create rule
         const validationRule = check(text);
 
-        //Add basic rules for text
+        //Add basic rules for text (Must be a regulated size string with only letters)
         validationRule.isString().withMessage(`${traduction} doit être une chaîne de caractères`).isLength({ min: min, max: max }).withMessage(`${traduction} doit avoir entre ${min} et ${max} caractères`).matches(/^[a-zA-Z\s]+$/).withMessage(`${traduction} ne doit contenir que des lettres et des espaces`);
 
         //Add conditionnal rules (require or not)
@@ -41,17 +43,27 @@ class ValidateBody{
         }
 
         //Push rules in rules array
-        this.addValidationRule(validationRule);    
+        this._addValidationRule(validationRule);    
     }
 
-    emailValidator(email, require, alreadyExist, existUser){
+    
+/**
++     * Validate email based on given conditions.
++     *
++     * @param {string} email - the key in body
++     * @param {boolean} require - whether the email is required
++     * @param {boolean} alreadyExist - whether the email must not exist in the database
++     * @param {boolean} existUser - whether the email must exist in the database
++     * @return {void}
++     */
+    async emailValidator(email, require, alreadyExist, existUser){
 
         //Create rule
         const validationRule = check(email);
 
-        // Add basic rules for email
+        // Add basic rules for email (must be an email)
         validationRule.isEmail().withMessage('L\'adresse e-mail est invalide').normalizeEmail()
-       
+    
         // Add conditionnal rules if email is required
         if (require) {
             validationRule.notEmpty().withMessage('L\'adresse e-mail est obligatoire');
@@ -75,31 +87,44 @@ class ValidateBody{
                 // Check if user exist with the given email
                 const user = await User.findOne({ email: value });
                 if (!user) {
-                    throw new Error('Cet utilisateur n\'existe pas');
+                        throw new Error('Informations d\'identification invalides');
                 }
                 return true;
-            })
+            });
         }
 
         // Push rules in rules array
-        this.addValidationRule(validationRule);  
+        this._addValidationRule(validationRule);
     }
 
+ /**
++     * Validates the given password according to the defined rules.
++     *
++     * @param {string} password - the key in body
++     * @return {void}
++     */
     passwordValidator(password){
         // Create rule
         const validationRule = check(password);
 
-        // Add basic rules for password
+        // Add basic rules for password (Must have a regulated size and have an uppercase letter, a number and a special character)
         validationRule.isLength({ min: 6, max: 20 }).withMessage('Le mot de passe doit avoir entre 6 et 20 caractères').matches(/^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/).withMessage('Le mot de passe doit contenir au moins une majuscule, un chiffre et un caractère spécial');
 
         // Push rules in rules array
-        this.addValidationRule(validationRule); 
+        this._addValidationRule(validationRule); 
     }
 
+/**
++     * Role validator function.
++     *
++     * @param {string} role - the key in body
++     * @param {boolean} require - description of parameter
++     * @return {void}
++     */
     roleValidator(role, require){
         const validationRule = check(role);
 
-        // Add basic rules for role
+        // Add basic rules for role (Must be "user", "health" or "blog")
         validationRule.isIn(['user', 'health', 'blog']).withMessage('Le rôle doit être "user", "health" ou "blog"');
 
         // Add conditionnal rules (require or not)
@@ -108,14 +133,21 @@ class ValidateBody{
         }
 
         // Push rules in rules array
-        this.addValidationRule(validationRule);
+        this._addValidationRule(validationRule);
     }
 
+/**
++     * Validate a birthdate.
++     *
++     * @param {string} birthdate - the key in body
++     * @param {boolean} require - is require in body or not
++     * @return {void}
++     */
     birthdateValidator(birthdate, require){
         // Create rule
         const validationRule = check(birthdate);
 
-        // Add basic rules for birthdate
+        // Add basic rules for birthdate (Must be an ISO8601 date)  
         validationRule.isISO8601().toDate().withMessage('La date de naissance est au mauvais format');
 
         if (require) {
@@ -123,25 +155,45 @@ class ValidateBody{
         }
 
         // Push rules in rules array
-        this.addValidationRule(validationRule);
+        this._addValidationRule(validationRule);
     }
 
-    phoneValidator(phone, require){
+/**
++     * Validate a phone number and add validation rule.
++     *
++     * @param {string} phone - the key in body
++     * @param {boolean} require - is require in body or not
++     * @return {void}
++     */
+    phoneValidator(phone, require = false){
         // Create rule
         const validationRule = check(phone);
 
-        // Add basic rules for phone
-        validationRule.optional().isMobilePhone('any', {strictMode : false}).withMessage('Le numéro de téléphone doit être valide');
+        // Add basic rules for phone (Must be a mobile phone)
+        validationRule.isMobilePhone('any', {strictMode : false}).withMessage('Le numéro de téléphone doit être valide');
+        if (require) {
+            validationRule.notEmpty().withMessage('Le numéro de numéro est obligatoire');
+        }else{
+            validationRule.optional();
+        }
 
         // Push rules in rules array
-        this.addValidationRule(validationRule);
+        this._addValidationRule(validationRule);
     }
     
+/**
++     * Validate a secret pin.
++     *
++     * @param {string} secret_pin - the key in body
++     * @param {boolean} require - is require in body or not
++     * @param {number} lenght - lenght of secret pin
++     * @return {void}
++     */
     secretPinValidator(secret_pin, require, lenght){
         // Create rule
         const validationRule = check(secret_pin);
 
-        // Add basic rules for secret_pin
+        // Add basic rules for secret_pin (must be a number with a regulated size)
         validationRule.isNumeric().withMessage('Le code secret doit être un nombre').isLength({ min: lenght, max: lenght }).withMessage('Le code secret doit avoir exactement 6 chiffres');
 
         // Add conditionnal rules (require or not)
@@ -150,7 +202,33 @@ class ValidateBody{
         }
 
         // Push rules in rules array
-        this.addValidationRule(validationRule);
+        this._addValidationRule(validationRule);
+    }
+
+/**
++     * Check the user password with password in body.
++     *
++     * @param {string} password - the key in body
++     * @param {User} user - user for comparison
++     * @return {void}
++     */
+    checkPassword(password, user){
+        // Create rule
+        const validationRule = check(password);
+
+        // Password must be the same as user
+        validationRule.custom(async (value) => {
+            // Compare password
+            const passwordMatching = await bcrypt.compare(value, user.password);
+            if (!passwordMatching) {
+                throw new Error('Informations d\'identification invalides');
+            }
+            // Password are the same
+            return true;
+        });
+
+        // Push rules in rules array
+        this._addValidationRule(validationRule);
     }
 
     //////////
@@ -159,9 +237,15 @@ class ValidateBody{
     //////////
     //PUSH RULES
 
+/**
++     * Push new rules in validation rules array.
++     *
++     * @param {ValidationChain} rule - new rule
++     * @return {void}
++     */
     // Push new rules in validation rules array
-    addValidationRule(rule) {
-        this.validationRules.push(rule);
+    _addValidationRule(rule) {
+        this._validationRules.push(rule);
     }
 
     //////////
@@ -170,10 +254,16 @@ class ValidateBody{
     //////////
     //VALIDATE RULES
 
-    validateRules = async ( req, res, notFoundMessage = "") => {
+/**
++     * Asynchronously apply all validation rules to the query fields
++     *
++     * @param {any} req - req
++     * @return {Result<ValidationError>} - return the validation results
++     */
+    async validateRules(req){
 
         // Asynchronously apply all validation rules to the query fields
-        await Promise.all(this.validationRules.map(validationRule => validationRule.run(req)));
+        await Promise.all(this._validationRules.map(validationRule => validationRule.run(req)));
     
         // Get the validation results for the query
         return validationResult(req);
