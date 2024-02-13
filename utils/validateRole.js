e910d22a691ffb47f30e3dd63eeed1eb70024f87
Paul_Delamare <paulodela2006@gmail.com>
2024-02-13 17:11:34 +0100

    Add in req user the user information

--- utils/validateRole.js
+++ utils/validateRole.js
@@ -11,18 +11,21 @@ const checkAuthAndRole = (role = "") => {
         // Check JWT authentication
         passport.authenticate('jwt', { session: false }, (err, user, info) => {
             if (err) {
-                return res.status(500).json({ error: 'Erreur interne du serveur' });
+                return res.status(500).json({ error: 'Erreur interne du serveur', status : 500 });
             }
             
             // If no user is authenticated or the JWT is invalid, return an error
             if (!user) {
-                return res.status(401).json({ error: 'Non authentifié. Vous devez vous connecter.' });
+                return res.status(401).json({ error: 'Non authentifié. Vous devez vous connecter.', status : 401 });
             }
 
             // Check if the user has the required role
             if (role && user.role !== role) {
-                return res.status(403).json({ error: 'Accès interdit. Vous n\'avez pas le rôle nécessaire.' });
+                return res.status(403).json({ error: 'Accès interdit. Vous n\'avez pas le rôle nécessaire.', status : 403 });
             }
+
+            //Stock in req.user user information
+            req.user = user;
 
             // If the user has the required role or no role is specified, continue to the next middleware
             next();
