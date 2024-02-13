ba4642e42b5cfa66accad2e6343687d5915c3350
Paul_Delamare <paulodela2006@gmail.com>
2024-02-13 11:30:07 +0100

    Create popup model

---
+++ models/Popup.js
@@ -0,0 +1,54 @@
+//////////
+//REQUIRE
+const mongoose = require('mongoose');
+//////////
+//////////
+
+//////////
+//SCHEMA
+const PopupSchema = new mongoose.Schema({
+    image_path: {
+        type: String,
+        required: true,
+    },
+    text: {
+        type : String,
+        required: true
+    }
+});
+
+//////////
+//////////
+
+//////////
+//MODEL
+
+const Popup = mongoose.model('Popup', PopupSchema, 'daily_popup');
+
+//////////
+//////////
+
+//////////
+//FUNCTIONS
+
+Popup.create = async (newPopup, resulte) => {
+    //Get body information
+    const {image_path, text} = newPopup;
+
+    //Create user object
+    const daily_popup = new Popup({
+        image_path,
+        text,
+    });
+
+    //Push in data base
+    return daily_popup.save();
+}
+
+//////////
+//////////
+
+//////////
+//EXPORT
+module.exports = Popup;
+//////////
