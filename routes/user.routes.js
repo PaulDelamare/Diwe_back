632df3d7b83192c4ae5b5a466127338df8966110
Paul_Delamare <paulodela2006@gmail.com>
2024-02-29 21:57:54 +0100

    Add route for update user information

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -20,6 +20,8 @@ const checkConnection = require ('../utils/validateRole');
 router.get('/user/last-connection', checkConnection(), UserController.checkLastConnection);
 //Route update profile picture
 router.post('/user/update-profile-picture', checkConnection(), upload, UserController.updateProfilePicture);
+//Route update profile picture
+router.put('/user/update-information', checkConnection(), UserController.changeInformation);
 //Change password
 router.put('/user/update-password', checkConnection(), UserController.changePassword);
 //Change email
