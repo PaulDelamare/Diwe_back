e560e7401f042a0ffb5ea6cd558ffcc8c75928a9
Paul_Delamare <paulodela2006@gmail.com>
2024-03-27 22:56:13 +0100

    Create model for medical data

---
+++ models/MedicalData.js
@@ -0,0 +1,44 @@
+//////////
+//REQUIRE
+const mongoose = require('mongoose');
+//////////
+//////////
+
+//////////
+//SCHEMA
+
+const MedicalSchema = new mongoose.Schema({
+    id_user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
+    date:{
+        type : Date,
+        default: Date.now
+    },
+    pulse:{
+        type: Number,
+        required: true
+    },
+    oxygen: {
+        type: Number,
+        required: true
+    }
+});
+
+//////////
+//////////
+
+//////////
+//MODEL
+
+const MedicalData = mongoose.model('MedicalData', MedicalSchema, 'medicalData');
+
+//////////
+//////////
+
+//////////
+//EXPORT
+module.exports = MedicalData;
+//////////
